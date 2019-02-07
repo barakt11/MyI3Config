d2ca8204588c706c24d36a5df8f8fed81114ce06
Barak Tawily <barakt@wix.com>
2019-02-07 17:12:20 +0200

    changes

--- scripts/changeWallpaper.sh
+++ scripts/changeWallpaper.sh
@@ -6,6 +6,13 @@ CURL_CMD="curl -s"
 
 # Selects a random wallpaper from the Chromecast feed
 html_page=$($CURL_CMD https://clients3.google.com/cast/chromecast/home)
+# needs to validate that we have a valid response!!!
+if [[ "$html_page" == "" ]]
+	then
+		echo "not internet connection";
+		exit
+fi
+
 json_wallpapers=$(sed -nr "s/.+JSON\.parse\('(.+?)\\\n'\).+/\1/p" <<< $html_page)
 clean_json=$(xargs -0 echo -e <<< $json_wallpapers)
 wallpaper_list=$(jq -r '.[0][] | .[0]' <<< $clean_json)
@@ -14,6 +21,7 @@ random_wallpaper=$(head -n1 <<< $random_wp_list | cut -d " " -f1)
 
 # Select a higher quality wallpaper (the default for Chromecasts is a downscaled version)
 quality_wp=$(sed -e "s/s1280-w1280-h720/s1920-w1920-h1200/" <<< $random_wallpaper)
+echo "$CURL_CMD $quality_wp -o $TMP_WP_LOCATION"
 # Download the wallpaper to a temporary location
 $CURL_CMD $quality_wp -o $TMP_WP_LOCATION
 
