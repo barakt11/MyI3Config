5c8083dc0ae1ea6ca967f78427e09002aa90f695
Barak Tawily <quitten11@gmail.com>
2019-05-16 19:47:07 +0300

    Update burp.sh

--- scripts/burp.sh
+++ scripts/burp.sh
@@ -1,6 +1,20 @@
+RES=$(python ~/scripts/focusOn.py "Burp Suite")
+echo $RES
+if [ $RES -eq "1" ]; then
+	exit
+fi
+
+ps aux | grep -i "java -jar .*burp.*jar$" | wc -l
 cd ~/softwares/burpsuite/
 BURPSTR="burpsuite_pro_v2.0."
 RES=$(ls ~/softwares/burpsuite/ | grep -oP "(?<=$BURPSTR).*(?=.jar)")
 RES=$(echo $RES | awk '{print $NF}')
 BURPJAR=$(ls ~/softwares/burpsuite/ | grep $BURPSTR$RES)
-java -jar ~/softwares/burpsuite/$BURPJAR
+
+SCREENUM=$(xrandr | grep ' connected' | wc | awk '{print $1}')
+echo $SCREENUM
+if [ $SCREENUM -eq 1 ]; then
+	GDK_DPI_SCALE=1 GDK_SCALE=2 /usr/lib/jvm/java-9-jre/jre/bin/java -jar ~/softwares/burpsuite/$BURPJAR
+else
+	GDK_DPI_SCALE=0 GDK_SCALE=1 /usr/lib/jvm/java-9-jre/jre/bin/java -jar ~/softwares/burpsuite/$BURPJAR
+fi
