fc4492a406a215abb875056073c6815e305b57c4
Barak Tawily <quitten11@gmail.com>
2019-06-19 16:06:36 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 sudo rsync -aAXv --progress \
-  --exclude={"/usr/lib/*","*/node_modules/*","/home/*/Downloads/ffsrc/*","/home/*/.npm/*","/home/*/.m2/*","/home/*/.config/chromium/","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
+  --exclude={"/var/lib/*","/usr/share/*","/usr/lib/*","*/node_modules/*","/home/*/Downloads/ffsrc/*","/home/*/.npm/*","/home/*/.m2/*","/home/*/.config/chromium/","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
   / /mnt/disk/
