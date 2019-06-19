c2c7daed0711e985d6d398f8df7a69b7de9228e4
Barak Tawily <quitten11@gmail.com>
2019-06-19 16:05:25 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 sudo rsync -aAXv --progress \
-  --exclude={"/home/*/.npm/*","/home/*/.m2/*","/home/*/.config/chromium/","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
+  --exclude={"/usr/lib/*","*/node_modules/*","/home/*/Downloads/ffsrc/*","/home/*/.npm/*","/home/*/.m2/*","/home/*/.config/chromium/","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
   / /mnt/disk/
