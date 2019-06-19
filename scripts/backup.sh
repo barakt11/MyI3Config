a8997f502f4487526722874c49ed4face6871df3
Barak Tawily <quitten11@gmail.com>
2019-06-19 16:01:55 +0300

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,3 @@
 sudo rsync -aAXv --progress \
-  --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
-  / /mnt/backup_drive/
+  --exclude={"/home/*/.npm/*","/home/*/.m2/*","/home/*/.config/chromium/","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","*/caches/*","*/cache/*","*/Cache/*","*/.cache*","*/ShaderCache/*","/home/*cache*","/home/*/.thumbnails/*","/home/*/.local/share/Trash/*","/home/*/.thumbnails/*"} \
+  / /mnt/disk/
